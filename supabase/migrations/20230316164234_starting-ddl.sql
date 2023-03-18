7355dfce98c2c22be82d87d87461f5afc451ecbb
ousama <ousawack@gmail.com>
2023-03-18 23:10:47 +0100

    typo fix

--- supabase/migrations/20230316164234_starting-ddl.sql
+++ supabase/migrations/20230316164234_starting-ddl.sql
@@ -1,7 +1,7 @@
 create table user_profiles (
-    user_id uuid primary key references auth.users(id) not null,
-    username text unique not null CONSTRAINT proper_username CHECK (username ~* "^[a-zA-Z0-9_]+$") CONSTRAINT username_length CHECK (
+    user_id uuid primary key references auth.users (id) not null,
+    username text unique not null CONSTRAINT proper_username CHECK (username ~* '^[a-zA-Z0-9_]+$') CONSTRAINT username_length CHECK (
         char_length(username) > 3
         and char_length(username) < 15
     )
-)
+);
