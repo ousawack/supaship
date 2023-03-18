aabb966206ac76f81321c3c5645b05bc9d5bc9d7
ousama <ousawack@gmail.com>
2023-03-19 00:00:05 +0100

    enable RLS

--- supabase/migrations/20230316164234_starting-ddl.sql
+++ supabase/migrations/20230316164234_starting-ddl.sql
@@ -1,7 +1,27 @@
 create table user_profiles (
     user_id uuid primary key references auth.users (id) not null,
-    username text unique not null CONSTRAINT proper_username CHECK (username ~* '^[a-zA-Z0-9_]+$') CONSTRAINT username_length CHECK (
+    username text unique not null 
+    CONSTRAINT proper_username CHECK (username ~* '^[a-zA-Z0-9_]+$') 
+    CONSTRAINT username_length CHECK (
         char_length(username) > 3
         and char_length(username) < 15
     )
 );
+
+alter table user_profiles enable row level security;
+
+CREATE POLICY "all can see" ON "public"."user_profiles"
+AS PERMISSIVE FOR SELECT
+TO public
+USING (true);
+
+CREATE POLICY "users can insert" ON "public"."user_profiles"
+AS PERMISSIVE FOR INSERT
+TO public
+WITH CHECK (auth.uid() = user.id);
+
+CREATE POLICY "owners can update" ON "public"."user_profiles"
+AS PERMISSIVE FOR UPDATE
+to public
+USING (auth.uid() = user.id)
+WITH CHECK (auth.uid() = user.id);
